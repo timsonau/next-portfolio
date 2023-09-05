a7dffe83fc83bfc615e511febee29cd7a34d0be9
timsonau <117775073+timsonau@users.noreply.github.com>
2023-09-04 20:33:05 -0500

    fix apos

--- public/data/experiencesData.tsx
+++ public/data/experiencesData.tsx
@@ -8,7 +8,7 @@ const experiences = [
     name: "Auburn University",
     dates: "08/2019 - 05/2023",
     description:
-      "Graduated Summa Cum Laude from Auburn University with Bachelor&apos;s in Computer Science with a concentration in Mathematics and Statistics.Participated in organization such as ACM, VSA, RUF-I Soccer, and AI club for which I held a leadership position. Relevant course works include Data Structures and Algorithms, Linear Algebra, Statistical Analysis, Programming Languages, Operating System, Modeling and Design, Data Mining, Computer Networks, Quality Assurance, Software Process, Digital Logic Circuits. GPA (4.0 / 4.0)",
+      "Graduated Summa Cum Laude from Auburn University with Bachelor's in Computer Science with a concentration in Mathematics and Statistics.Participated in organization such as ACM, VSA, RUF-I Soccer, and AI club for which I held a leadership position. Relevant course works include Data Structures and Algorithms, Linear Algebra, Statistical Analysis, Programming Languages, Operating System, Modeling and Design, Data Mining, Computer Networks, Quality Assurance, Software Process, Digital Logic Circuits. GPA (4.0 / 4.0)",
   },
 ];
 
